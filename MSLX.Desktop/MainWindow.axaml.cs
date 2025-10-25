64f62cbc204bffbf0a89ba140459a2349ae007a2
Waheal <2035582067@qq.com>
2025-10-25 12:08:26 +0800

    Add MSLX.Desktop

---
+++ MSLX.Desktop/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace MSLX.Desktop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
