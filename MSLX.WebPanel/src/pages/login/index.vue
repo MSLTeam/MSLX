7a11885d1b3155b7aa1e8e547280c4625b242811
“luluxiaoyu” <ljy.xiaoyu@qq.com>
2025-10-29 14:42:02 +0800

    style(webpanel): 重构登录页面样式与布局

--- MSLX.WebPanel/src/pages/login/index.vue
+++ MSLX.WebPanel/src/pages/login/index.vue
@@ -4,7 +4,6 @@ export default {
 };
 </script>
 <script setup lang="ts">
-
 import Login from './components/Login.vue';
 import LoginHeader from './components/Header.vue';
 import TdesignSetting from '@/layouts/setting.vue';
@@ -13,20 +12,23 @@ import TdesignSetting from '@/layouts/setting.vue';
   <div class="login-wrapper">
     <login-header />
 
-    <div class="login-container">
-      <div class="title-container">
-        <h1 class="title margin-no">登录到</h1>
-        <h1 class="title">MSLX 网页管理中心</h1>
+    <div class="login-panel">
+      <div class="login-container">
+        <div class="title-container">
+          <h1 class="title margin-no">连接到</h1>
+          <h1 class="title">MSLX 网页管理中心</h1>
+        </div>
+
+        <login />
+
+        <footer class="copyright">Copyright @ 2021-{{ new Date().getFullYear() }} MSLTeam. All Rights Reserved</footer>
       </div>
 
-      <login />
-      <tdesign-setting />
     </div>
 
-    <footer class="copyright">Copyright @ 2021-{{ new Date().getFullYear() }} MSLTeam. All Rights Reserved</footer>
+    <tdesign-setting class="tdesign-setting-outside" />
   </div>
 </template>
-
 
 <style lang="less" scoped>
 @import url('./index.less');
