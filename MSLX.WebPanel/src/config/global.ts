aedac0842d642442c22fbe37d518506772a49da8
“luluxiaoyu” <ljy.xiaoyu@qq.com>
2025-10-30 11:35:27 +0800

    style(webpanel): 优化移动端 & pc端内容区内边距

--- MSLX.WebPanel/src/config/global.ts
+++ MSLX.WebPanel/src/config/global.ts
@@ -1,3 +1,3 @@
-export const prefix = 'tdesign-starter';
+export const prefix = 'mslx-webpanel';
 export const TOKEN_NAME = 'tdesign-starter';
 export const BASE_URL_NAME = 'tdesign-starter-base-url';
