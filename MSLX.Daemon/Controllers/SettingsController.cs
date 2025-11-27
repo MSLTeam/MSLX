aed24d5fcb8a17249a7b1eb2cbd7b7967e861dce
“luluxiaoyu” <ljy.xiaoyu@qq.com>
2025-11-27 23:37:29 +0800

    feat(daemon): 添加获取/修改MSLX本题设置的功能

---
+++ MSLX.Daemon/Controllers/SettingsController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using MSLX.Daemon.Models;
+using MSLX.Daemon.Models.Settings;
+using MSLX.Daemon.Utils;
+
+namespace MSLX.Daemon.Controllers;
+
+[ApiController]
+[Route("api/settings")]
+public class SettingsController: ControllerBase 
+{
+    [HttpGet]
+    public IActionResult GetSettings()
+    {
+        return Ok(new ApiResponse<object>
+            {
+                Code = 200,
+                Message = "获取成功",
+                Data = new
+                {
+                    User = ConfigServices.Config.ReadConfig()["user"]?? "",
+                    Avatar = ConfigServices.Config.ReadConfig()["avatar"]?? "",
+                    FireWallBanLocalAddr = ConfigServices.Config.ReadConfig()["fireWallBanLocalAddr"]?? false,
+                }
+            }
+        );
+    }
+    
+    [HttpPost]
+    public IActionResult UpdateSettings([FromBody] UpdateSettingsRequest request)
+    {
+        ConfigServices.Config.WriteConfigKey("user", request.User);
+        ConfigServices.Config.WriteConfigKey("avatar", request.Avatar);
+        ConfigServices.Config.WriteConfigKey("fireWallBanLocalAddr", request.FireWallBanLocalAddr);
+        return Ok(new ApiResponse<object>
+            {
+                Code = 200,
+                Message = "更新成功",
+            }
+        );
+    }
+}
