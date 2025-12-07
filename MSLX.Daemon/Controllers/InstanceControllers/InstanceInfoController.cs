cb5d39bdb28baf5143d428b4cd72a5b30291b1f5
“luluxiaoyu” <ljy.xiaoyu@qq.com>
2025-12-07 13:39:50 +0800

    feat(daemon): 更新实例列表接口以包含运行状态

--- MSLX.Daemon/Controllers/InstanceControllers/InstanceInfoController.cs
+++ MSLX.Daemon/Controllers/InstanceControllers/InstanceInfoController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using MSLX.Daemon.Models;
+using MSLX.Daemon.Services;
 using MSLX.Daemon.Utils;
 using Newtonsoft.Json.Linq;
 
@@ -9,25 +10,39 @@ namespace MSLX.Daemon.Controllers.InstanceControllers;
 [ApiController]
 public class InstanceInfoController : ControllerBase
 {
+    private readonly MCServerService _mcServerService;
+    public InstanceInfoController(MCServerService mcServerService)
+    {
+        _mcServerService = mcServerService;
+    }
+    
     [HttpGet("list")]
     public IActionResult GetInstanceList()
     {
         ConfigServices.ServerListConfig config = ConfigServices.ServerList;
         var serverList = config.ReadServerList();
-        if (serverList != null)
+        var resultList = serverList.Select(item => 
         {
-            var response = new ApiResponse<JObject>
-            {
-                Code = 200,
-                Message = "ok",
-                Data = JObject.FromObject(new { list = serverList })
-            };
+            uint id = item["ID"]?.Value<uint>() ?? 0;
+            bool isRunning = _mcServerService.IsServerRunning(id);
 
-            return Ok(response);
-        }
-        else
+            return new 
+            {
+                id,
+                name = item["Name"]?.Value<string>(),
+                basePath = item["Base"]?.Value<string>(),
+                java = item["Java"]?.Value<string>(),
+                core = item["Core"]?.Value<string>(),
+                icon = "https://bbs-static.miyoushe.com/static/2024/12/05/0e139c6c04de1a23ea1400819574b59c_3322869863064933822.gif", // icon 暂时写死 日后再改
+                status = isRunning,
+            };
+        }).ToList();
+        
+        return Ok(new ApiResponse<object>
         {
-            return BadRequest(new { message = "Failed!" });
-        }
+            Code = 200,
+            Message = "获取成功",
+            Data = resultList
+        });
     }
 }
