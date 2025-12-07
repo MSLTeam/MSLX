95e1d2db828cf12f08ab8a30cc871815da5bc511
“luluxiaoyu” <ljy.xiaoyu@qq.com>
2025-12-07 14:33:35 +0800

    fix(webpanel): 部分pinia接口添加错误处理和用户提示

--- MSLX.WebPanel/src/store/modules/frp.ts
+++ MSLX.WebPanel/src/store/modules/frp.ts
@@ -2,12 +2,17 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue';
 import type { FrpListModel } from '@/api/model/frp';
 import { getFrpList } from '@/api/frp';
+import { MessagePlugin } from 'tdesign-vue-next';
 
 export const useTunnelsStore = defineStore('tunnels',()=> {
   const frpList = ref<FrpListModel[]>([]);
 
   async function getTunnels() {
-    frpList.value = await getFrpList();
+    try{
+      frpList.value = await getFrpList();
+    }catch (e){
+      MessagePlugin.error('获取Frp列表失败:'+e.message);
+    }
   }
 
   return {
--- MSLX.WebPanel/src/store/modules/instance.ts
+++ MSLX.WebPanel/src/store/modules/instance.ts
@@ -2,12 +2,18 @@ import { defineStore } from 'pinia';
 import { InstanceListModel } from '@/api/model/instance';
 import { ref } from 'vue';
 import { getInstanceList } from '@/api/instance';
+import { MessagePlugin } from 'tdesign-vue-next';
 
 export const useInstanceListstore = defineStore('instanceList',()=> {
   const instanceList = ref<InstanceListModel[]>([]);
 
   async function refreshInstanceList() {
-    instanceList.value = await getInstanceList();
+    try{
+      instanceList.value = await getInstanceList();
+    }catch (e){
+      MessagePlugin.error('获取实例列表失败:'+e.message);
+    }
+
   }
 
   return {
