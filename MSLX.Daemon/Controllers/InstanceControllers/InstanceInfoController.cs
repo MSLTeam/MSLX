a90f3c30d77a1c4b30b1066ae84669976dd87582
“luluxiaoyu” <ljy.xiaoyu@qq.com>
2025-12-07 21:48:27 +0800

    feat(webpanel): 添加获取单个服务器实例信息的接口

--- MSLX.Daemon/Controllers/InstanceControllers/InstanceInfoController.cs
+++ MSLX.Daemon/Controllers/InstanceControllers/InstanceInfoController.cs
@@ -45,4 +45,37 @@ public class InstanceInfoController : ControllerBase
             Data = resultList
         });
     }
+
+    [HttpGet("info")]
+    public IActionResult GetInstanceInfo(uint id)
+    {
+        try
+        {
+            McServerInfo.ServerInfo server =
+                ConfigServices.ServerList.GetServer(id) ?? throw new Exception("找不到指定的服务器");
+            return Ok(new ApiResponse<object>
+            {
+                Code = 200,
+                Message = "获取成功",
+                Data = new
+                {
+                    id = server.ID,
+                    name = server.Name,
+                    basePath = server.Base,
+                    java = server.Java,
+                    minM = server.MinM,
+                    maxM = server.MaxM,
+                    core = server.Core,
+                    status = _mcServerService.IsServerRunning(id),
+                }
+            });
+        }catch (Exception e)
+        {
+            return BadRequest(new ApiResponse<object>
+            {
+                Code = 400,
+                Message = e.Message
+            });
+        }
+    }
 }
