c75e987fbf2800311e134adbaf1bb73351b2d9bd
luluxiaoyu <ljy.xiaoyu@qq.com>
2025-12-14 17:05:32 +0800

    bumpver(webpanel & daemon): v0.2.0-alpha

--- MSLX.Daemon/Controllers/AppInfoController.cs
+++ MSLX.Daemon/Controllers/AppInfoController.cs
@@ -82,7 +82,7 @@ public class AppInfoController : ControllerBase
                 ["targetFrontendVersion"] = new JObject
                 {
                     ["desktop"] = "0.0.0",
-                    ["panel"] = "0.1.1"
+                    ["panel"] = "0.2.0"
                 },
                 ["systemInfo"] = systemInfo
             };
