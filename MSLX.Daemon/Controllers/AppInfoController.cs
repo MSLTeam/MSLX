e7baa436a39186b4b40029ff0f55352c34ca435e
luluxiaoyu <ljy.xiaoyu@qq.com>
2025-12-27 21:48:07 +0800

    bumpver(daemon & webpanel): 更新版本号到 v0.5.3.1-beta

--- MSLX.Daemon/Controllers/AppInfoController.cs
+++ MSLX.Daemon/Controllers/AppInfoController.cs
@@ -83,7 +83,7 @@ public class AppInfoController : ControllerBase
                 ["targetFrontendVersion"] = new JObject
                 {
                     ["desktop"] = "0.0.0",
-                    ["panel"] = "0.5.3"
+                    ["panel"] = "0.5.3.1"
                 },
                 ["systemInfo"] = systemInfo
             };
