75cb0db8e88b2e1c55bd9af6257ef82170e703e8
luluxiaoyu <ljy.xiaoyu@qq.com>
2026-01-17 00:17:18 +0800

    feat(webpanel & daemon): 新增版本更新提醒和下载功能

---
+++ MSLX.WebPanel/src/api/update.ts
@@ -0,0 +1,14 @@
+import { request } from '@/utils/request';
+import { UpdateDownloadInfoModel, UpdateInfoModel } from '@/api/model/update';
+
+export async function getDaemonUpdateInfo() {
+  return request.get<UpdateInfoModel>({
+    url: '/api/update/info',
+  });
+}
+
+export async function getDaemonUpdateDownloadInfo() {
+  return request.get<UpdateDownloadInfoModel>({
+    url: '/api/update/download',
+  });
+}
