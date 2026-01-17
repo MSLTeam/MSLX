7a9512f229d93f43e1fca84d454835f0ced653c7
luluxiaoyu <ljy.xiaoyu@qq.com>
2026-01-17 12:27:24 +0800

    assets(daemon): 嵌入 v1.0.0 版本前端资源

--- MSLX.Daemon/Frontend/assets/js/docs.CxArFray.js
+++
@@ -1,1 +0,0 @@
-const s={cron:"https://mslx.mslmc.cn/docs/server-config/crons",style:"https://mslx.mslmc.cn/docs/style/webpanel/",remote_access:"https://mslx.mslmc.cn/docs/config/remote-access/"};export{s as D};
