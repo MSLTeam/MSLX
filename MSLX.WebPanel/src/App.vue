df4865e05c1065f002206b7223a55a5bce16db9a
luluxiaoyu <ljy.xiaoyu@qq.com>
2026-01-20 22:07:10 +0800

    perf(webpanel): 更优雅的方式处理主题自动变更

--- MSLX.WebPanel/src/App.vue
+++ MSLX.WebPanel/src/App.vue
@@ -1,3 +1,36 @@
+<script setup lang="ts">
+import { computed, onMounted, onUnmounted } from 'vue';
+import { useSettingStore, useUpdateStore } from '@/store';
+import UpdateModal from '@/components/UpdateModal.vue';
+
+const store = useSettingStore();
+const updateStore = useUpdateStore();
+
+const mode = computed(() => store.displayMode);
+
+// 监听主题变化的对象
+const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+// handle变更主题
+const handleSystemChange = (e: MediaQueryListEvent) => {
+  store.setSystemTheme(e.matches ? 'dark' : 'light');
+};
+
+onMounted(() => {
+  updateStore.checkAppUpdate(false);
+
+  // 挂载监听
+  mediaQuery.addEventListener('change', handleSystemChange);
+
+  // 初始化校准
+  store.setSystemTheme(mediaQuery.matches ? 'dark' : 'light');
+});
+
+onUnmounted(() => {
+  mediaQuery.removeEventListener('change', handleSystemChange);
+});
+</script>
+
 <template>
   <router-view :class="[mode]" />
   <update-modal
@@ -8,48 +41,6 @@
     @skip="updateStore.handleSkipVersion"
   />
 </template>
-
-<script setup lang="ts">
-import { computed, onMounted, onUnmounted, nextTick } from 'vue';
-import { useSettingStore, useUpdateStore } from '@/store';
-import UpdateModal from '@/components/UpdateModal.vue';
-
-const store = useSettingStore();
-const updateStore = useUpdateStore();
-
-const mode = computed(() => {
-  return store.displayMode;
-});
-
-// 监听系统主题变化
-const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-
-const handleSystemChange = (e: MediaQueryListEvent) => {
-  // auto才操作
-  if (store.mode === 'auto') {
-    const isSystemDark = e.matches;
-
-    // 强制把 Store 设置成当前的系统色 (Dark/Light)
-    store.updateConfig({ ...store.$state, mode: isSystemDark ? 'dark' : 'light' });
-
-    // 改回 Auto
-    nextTick(() => {
-      store.updateConfig({ ...store.$state, mode: 'auto' });
-    });
-  }
-};
-
-onMounted(() => {
-  updateStore.checkAppUpdate(false);
-
-  // 挂载监听
-  mediaQuery.addEventListener('change', handleSystemChange);
-});
-
-onUnmounted(() => {
-  mediaQuery.removeEventListener('change', handleSystemChange);
-});
-</script>
 
 <style lang="less" scoped>
 #nprogress .bar {
