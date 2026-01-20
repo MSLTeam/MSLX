6cfea490f491f39b38e6d044dc0e38e687feb95d
luluxiaoyu <ljy.xiaoyu@qq.com>
2026-01-20 21:58:20 +0800

    feat(webpanel): 在UI主题设置为自动的时候，监听系统颜色主题的切换并自动切换UI的主题

--- MSLX.WebPanel/src/App.vue
+++ MSLX.WebPanel/src/App.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, onUnmounted, nextTick } from 'vue';
 import { useSettingStore, useUpdateStore } from '@/store';
 import UpdateModal from '@/components/UpdateModal.vue';
 
@@ -21,9 +21,33 @@ const mode = computed(() => {
   return store.displayMode;
 });
 
+// 监听系统主题变化
+const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+const handleSystemChange = (e: MediaQueryListEvent) => {
+  // auto才操作
+  if (store.mode === 'auto') {
+    const isSystemDark = e.matches;
+
+    // 强制把 Store 设置成当前的系统色 (Dark/Light)
+    store.updateConfig({ ...store.$state, mode: isSystemDark ? 'dark' : 'light' });
+
+    // 改回 Auto
+    nextTick(() => {
+      store.updateConfig({ ...store.$state, mode: 'auto' });
+    });
+  }
+};
+
 onMounted(() => {
-  // 检查更新
   updateStore.checkAppUpdate(false);
+
+  // 挂载监听
+  mediaQuery.addEventListener('change', handleSystemChange);
+});
+
+onUnmounted(() => {
+  mediaQuery.removeEventListener('change', handleSystemChange);
 });
 </script>
 
