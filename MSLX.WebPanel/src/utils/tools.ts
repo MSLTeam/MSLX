ada542f7e9ea0160048e3e90c02e301ddced1760
luluxiaoyu <ljy.xiaoyu@qq.com>
2026-02-04 15:43:09 +0800

    perf(webpanel): 使用本地回环地址时不显示MSL OAuth 配置项

--- MSLX.WebPanel/src/utils/tools.ts
+++ MSLX.WebPanel/src/utils/tools.ts
@@ -10,6 +10,14 @@ export function generateRandomString(length: number): string {
 }
 
 // 格式化时间戳
-export function formatTime (timestamp: number)  {
+export function formatTime(timestamp: number) {
   return new Date(timestamp * 1000).toLocaleString();
+}
+
+// 检测当前访问是否本地回环
+export function isLoopback(): boolean {
+  const hostname = window.location.hostname;
+  const ipv4Loopback = /^127\.\d+\.\d+\.\d+$/;
+
+  return hostname === 'localhost' || hostname === '::1' || hostname === '[::1]' || ipv4Loopback.test(hostname);
 }
