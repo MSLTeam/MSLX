1b5ebeee08d06b58284234c0eeb8ffbfb1da30e3
Waheal <2035582067@qq.com>
2026-02-09 16:12:10 +0800

    feat(desktop): 现在，链接daemon进程时，可以随地大小取消啦！

--- MSLX.Desktop/Views/CreateInstance/CreateMCServer.axaml.cs
+++ MSLX.Desktop/Views/CreateInstance/CreateMCServer.axaml.cs
@@ -218,17 +218,17 @@ public partial class CreateMCServer : UserControl
         {
             Title = "选择服务端核心文件",
             AllowMultiple = false,
-            FileTypeFilter = new[]
-            {
+            FileTypeFilter =
+            [
                 new FilePickerFileType("服务端文件")
                 {
-                    Patterns = new[] { "*.jar" }
+                    Patterns = ["*.jar"]
                 },
                 new FilePickerFileType("所有文件")
                 {
-                    Patterns = new[] { "*" }
+                    Patterns = ["*"]
                 }
-            }
+            ]
         });
 
         if (files.Count > 0)
@@ -393,17 +393,17 @@ public partial class CreateMCServer : UserControl
         {
             Title = "选择Java可执行文件",
             AllowMultiple = false,
-            FileTypeFilter = new[]
-            {
+            FileTypeFilter =
+            [
                 new FilePickerFileType("Java可执行文件")
                 {
-                    Patterns = new[] { "java", "java.exe" }
+                    Patterns = ["java", "java.exe"]
                 },
                 new FilePickerFileType("所有文件")
                 {
-                    Patterns = new[] { "*" }
+                    Patterns = ["*"]
                 }
-            }
+            ]
         });
 
         if (files.Count > 0)
