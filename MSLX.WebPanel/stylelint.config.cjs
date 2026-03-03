d1ed5e0393dad4be9316dc273fb78e8fb6c8d43d
luluxiaoyu <ljy.xiaoyu@qq.com>
2026-03-03 23:28:39 +0800

    chore(webpanel): 修正 stylelint 规则

--- MSLX.WebPanel/stylelint.config.cjs
+++ MSLX.WebPanel/stylelint.config.cjs
@@ -1,5 +1,15 @@
 module.exports = {
   defaultSeverity: 'error',
-  extends: ['stylelint-config-prettier'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-standard-less'
+  ],
   plugins: ['stylelint-less'],
+  rules: {
+    'at-rule-empty-line-before': null,
+    'custom-property-empty-line-before': null,
+    'declaration-empty-line-before': null,
+    'no-descending-specificity': null,
+    'at-rule-no-unknown': null,
+  },
 };
