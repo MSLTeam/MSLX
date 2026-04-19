826801d364df58e97e0d71b74af51d4d9c0b7de6
luluxiaoyu <ljy.xiaoyu@qq.com>
2026-04-19 22:12:44 +0800

    fix(daemon): 修复更新过程停止实例的阶段传递了错误的状态标识

--- MSLX.Daemon/Controllers/AppInfoController.cs
+++ MSLX.Daemon/Controllers/AppInfoController.cs
@@ -366,10 +366,10 @@ public class AppInfoController : ControllerBase
             {
                 if (_serverService.HasRunningServers())
                 {
-                    await SendUpdateProgressAsync(100, "0 KB/s", "restarting", "正在关闭运行中的实例...");
+                    await SendUpdateProgressAsync(100, "0 KB/s", "preparing", "正在关闭运行中的实例...");
                     _serverService.StopAllServers();
                 }
-                await SendUpdateProgressAsync(100, "0 KB/s", "restarting", "准备重启守护进程...");
+                await SendUpdateProgressAsync(100, "0 KB/s", "preparing", "准备重启守护进程...");
                 await StartUpdateScriptAndExitAsync(newFileTempName, isWindows);
             }
             else
