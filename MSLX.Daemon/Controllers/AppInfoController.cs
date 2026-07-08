6450436bdcea76acfe2c48443fc043cee8e9d62a
luluxiaoyu <ljy.xiaoyu@qq.com>
2026-07-08 12:38:07 +0800

    bumpver(daemon & webpanel): 更新到v1.4.10.1版本 & 修复前端资源错版问题

--- MSLX.Daemon/Controllers/AppInfoController.cs
+++ MSLX.Daemon/Controllers/AppInfoController.cs
@@ -105,7 +105,7 @@ public class AppInfoController : ControllerBase
                 ["targetFrontendVersion"] = new JObject
                 {
                     ["desktop"] = "1.0.0",
-                    ["panel"] = "1.4.10"
+                    ["panel"] = "1.4.10.1"
                 },
                 ["systemInfo"] = systemInfo
             };
